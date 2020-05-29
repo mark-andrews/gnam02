e5b3d6a2166e53c45b8f3054b78ed4decbcaa1c6
Mark Andrews <mjandrews.org@gmail.com>
2020-05-29 10:27:40 +0100

    Add gam_script3

---
+++ scripts/gam_script3.R
@@ -0,0 +1,53 @@
+## ------------------------------------------------------------------------------------------------------------------------
+library(tidyverse)
+library(splines)
+library(lme4)
+
+# Understanding linear mixed effects
+
+Msleep <- lmer(Reaction ~ Days + (Days|Subject),
+               data = sleepstudy)
+
+sleepstudy %>% 
+  add_predictions(Msleep) %>% 
+  ggplot(aes(x = Days, y = Reaction, col = Subject)) + 
+    geom_point() + 
+    geom_line(aes(y = pred)) +
+    facet_wrap(~Subject)
+
+
+# Load random effects tanh data sets --------------------------------------
+data_df <- read_csv('data/raneftanh.csv')
+
+# plot it -----------------------------------------------------------------
+ggplot(data_df,
+       aes(x = x, y = y, col = v)) + geom_point() + facet_wrap(~v)
+
+# cubic b-spline expansion ------------------------------------------------
+
+add_basis_functions <- function(x, k = 5){
+  bs(x, degree = k) %>%
+    as_tibble() %>% 
+    rename_all(~paste0('phi_', .)) %>%
+    mutate_all(as.numeric)
+}
+bs_df <- data_df %>% pull(y) %>% add_basis_functions(k = 5)
+data_df <- bind_cols(data_df, bs_df)
+
+# look at the basis functions
+data_df %>% 
+  select(y, starts_with('phi')) %>% 
+  pivot_longer(cols = -y,
+               names_to = 'phi',
+               values_to = 'f') %>% 
+  ggplot(aes(x = y, y = f, colour = phi)) + geom_line()
+
+# Use lmer
+M <- lmer(y ~ phi_1 + phi_2 + (phi_1 + phi_2|v), 
+          data = data_df)
+
+# Use predictions
+data_df %>% 
+  add_predictions(M) %>% 
+  ggplot(aes(x = x, y = pred, colour = v)) +
+    geom_point() + facet_wrap(~v)
